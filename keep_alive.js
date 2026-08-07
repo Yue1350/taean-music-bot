56345c7a537cfaf3d113fb0dd1591137ccd1fd29
Yue1350 <a01063790943@gmail.com>
2026-08-07 10:05:36 +0900

    Create keep_alive.js

---
+++ keep_alive.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+function keepAlive() {
+    const server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('Bot is running!');
+    });
+
+    const PORT = process.env.PORT || 8080;
+    
+    server.listen(PORT, '0.0.0.0', () => {
+        console.log(`웹 서버가 포트 ${PORT}번에서 실행 중이야!`);
+    });
+}
+
+module.exports = keepAlive;
