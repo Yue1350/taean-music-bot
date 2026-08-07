d85f083001662e31a0532f7b13a2e04cabbe845e
Yue1350 <a01063790943@gmail.com>
2026-08-07 21:44:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -47,10 +47,9 @@ setupMusicEvents(client);
 client.once('ready', async () => {
     console.log(`[Bot] ${client.user.tag} 로 로그인 완료!`);
 
-    // Lavalink 매니저 초기화시 유저 정보 전달
-    await client.lavalink.init({
-        id: client.user.id,
-        username: client.user.username
+    // client.user 객체를 통째로 전달
+    await client.lavalink.init(client.user);
+});
     });
 });
 
